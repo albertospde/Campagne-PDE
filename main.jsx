6ef5b5d93e791f2d682d480a8a8ba22b807ef363
albertospde <albertospde@gmail.com>
2026-06-16 09:03:48 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Campagne from './Campagne.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Campagne />
+  </React.StrictMode>
+)
